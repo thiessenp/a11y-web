7787d94f2150f15c80efdd34948b5b85e0bcba3a
thiessenp <thiessenp@gmail.com>
2021-02-08 07:24:34 -0800

    Linting Tablist

--- webpack.config.js
+++ webpack.config.js
@@ -2,74 +2,74 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        // Source file (main) path to bundle.
-        //
-        // -Could also- define as individual objects (chunks)
-        // main...
-        // keynav: path.resolve(__dirname, "src/keynav.js"),
-        // hotkeys: path.resolve(__dirname, "src/hotkeys.js"),
-        main: path.resolve(__dirname, "lib/index.js"),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist/'),
-        filename: 'index.js',
-        
-        // Var/Module default name lib exposed as.
-        // DEBUG: WK suddenly missing sub modules? Try update name, build, undo build
-        library: 'A11yWeb',
+  entry: {
+    // Source file (main) path to bundle.
+    //
+    // -Could also- define as individual objects (chunks)
+    // main...
+    // keynav: path.resolve(__dirname, "src/keynav.js"),
+    // hotkeys: path.resolve(__dirname, "src/hotkeys.js"),
+    main: path.resolve(__dirname, 'lib/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'index.js',
 
-        // umd = Works with CommonJS and ES Modules
-        libraryTarget: 'umd',
+    // Var/Module default name lib exposed as.
+    // DEBUG: WK suddenly missing sub modules? Try update name, build, undo build
+    library: 'A11yWeb',
 
-        // Note: bellow may not be needed but may help
-        // libraryExport: 'default',       
-        // umdNamedDefine: true
+    // umd = Works with CommonJS and ES Modules
+    libraryTarget: 'umd',
+
+    // Note: bellow may not be needed but may help
+    // libraryExport: 'default',
+    // umdNamedDefine: true
+  },
+  // Externalize these dependencies to reduce bundle size
+  externals: {
+    // Debugging: Didn't help
+    // 'keynav-web': {
+    //     commonjs: 'keynav-web',
+    //     commonjs2: 'keynav-web',
+    //     umd: 'keynav-web'
+    // },
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_',
     },
-    // Externalize these dependencies to reduce bundle size
-    externals: {
-        // Debugging: Didn't help
-        // 'keynav-web': {
-        //     commonjs: 'keynav-web',
-        //     commonjs2: 'keynav-web',
-        //     umd: 'keynav-web'
-        // },
-        lodash: {
-            commonjs: 'lodash',
-            commonjs2: 'lodash',
-            amd: 'lodash',
-            root: '_',
-        },
-    },
-	module: {
-        // Processors
-        rules: [
-            // Run Babel for any JS files (exluding node_modules)
-            {
-                test: /\.(js)$/,
-                
-                // Debuggint: commenting out gives the same error, diff path
-                exclude: /node_modules/,
-                
-                // Debugging: didn't help
-                //include: /node_modules\/keynav-web\/src/,
-                
-                use: "babel-loader",
-            },
-            // Images
-            //   {
-            //     test: /.png$/,
-            //     use: 'base64-image-loader'
-            //   },
-            // CSS
-            //   {
-            //     test: /.css$/,
-            //     use: 'css-content-loader'
-            //   }
-            // Many others
-        ]
-    },
-    // Development to allow viewing the source
-    // IMPORTANT: build step should compress with `--mode=production`
-    mode: "development", 
+  },
+  module: {
+    // Processors
+    rules: [
+      // Run Babel for any JS files (exluding node_modules)
+      {
+        test: /\.(js)$/,
+
+        // Debuggint: commenting out gives the same error, diff path
+        exclude: /node_modules/,
+
+        // Debugging: didn't help
+        // include: /node_modules\/keynav-web\/src/,
+
+        use: 'babel-loader',
+      },
+      // Images
+      //   {
+      //     test: /.png$/,
+      //     use: 'base64-image-loader'
+      //   },
+      // CSS
+      //   {
+      //     test: /.css$/,
+      //     use: 'css-content-loader'
+      //   }
+      // Many others
+    ],
+  },
+  // Development to allow viewing the source
+  // IMPORTANT: build step should compress with `--mode=production`
+  mode: 'development',
 };
